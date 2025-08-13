9b1c079e40994716f8fbe9d721bb05edb1f96ace
democlarityvoice-del <demo.clarityvoice@gmail.com>
2025-08-12 20:15:21 -0400

    Update smartrouting.js

--- smartrouting.js
+++ smartrouting.js
@@ -11,3 +11,19 @@
     let $template = $('#nav-music');
     if (!$template.length) $template = $('#nav-buttons').children('li').first();
     if (!$template.length) return;
+
+const $new = $template.clone();
+    $new.attr('id', 'nav-intelli-routing');
+    $new.find('a').attr('id', 'nav-Intelli-Routing').attr('href', '#');
+    $new.find('.nav-text').html('EngageCX');
+    $new.find('.nav-bg-image').css({
+      '-webkit-mask-image': "url('https://raw.githubusercontent.com/democlarityvoice-del/engagecxicon/main/message-regular-full.svg?v=3')",
+      'mask-image':         "url('https://raw.githubusercontent.com/democlarityvoice-del/engagecxicon/main/message-regular-full.svg?v=3')",
+      '-webkit-mask-repeat': 'no-repeat',
+      'mask-repeat':         'no-repeat',
+      '-webkit-mask-position':'center 48%',
+      'mask-position':       'center 48%',
+      '-webkit-mask-size':   '71% 71%',
+      'mask-size':           '71% 71%',
+      'background-color':    'rgba(255,255,255,0.92)'
+    });
